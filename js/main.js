74686438b1af069cadb2ef87abe5b693031b42df
sameer <sameer678sahu@gmail.com>
2022-11-09 20:41:33 +0530

    Margin and Group Implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
+
 const svg = d3.select('#chart-area').append('svg')
-  .attr('width', 600)
-  .attr('height', 500)
+  .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
 d3.csv('data/revenues.csv').then((data) => {
   data.forEach((d) => {
@@ -10,15 +17,15 @@ d3.csv('data/revenues.csv').then((data) => {
 
   const x = d3.scaleBand()
   .domain(data.map(d => d.month))
-  .range([0, 600])
+  .range([0, WIDTH])
   .paddingInner(0.3)
   .paddingOuter(0.2)
 
   const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.revenue)])
-    .range([0, 500])
+    .range([0, HEIGHT])
 
-  const rect = svg.selectAll('rect').data(data)
+  const rect = g.selectAll('rect').data(data)
 
   rect
     .enter()
